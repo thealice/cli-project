bb87a5a9251aebcdb7c1bddd9d75612b37688079
Alice Freda <alicefreda@gmail.com>
2019-11-19 19:51:50 +0000

    initial commit

---
+++ spec/cli_project_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe CliProject do
+  it "has a version number" do
+    expect(CliProject::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
