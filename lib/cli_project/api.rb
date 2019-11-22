2ca3f104c370140b21f66b3736512bf870306621
Alice Freda <alicefreda@gmail.com>
2019-11-22 02:14:03 +0000

    setup gets for films and species

--- lib/cli_project/api.rb
+++ lib/cli_project/api.rb
@@ -2,19 +2,17 @@ class API
 
   include HTTParty
 
-  attr_accessor :name
-
   base_uri 'ghibliapi.herokuapp.com'
 
   def self.get_films
-    response = HTTParty.get('https://ghibliapi.herokuapp.com/films')
-    binding.pry
-    puts response.parsed_response
+    films_array = HTTParty.get('https://ghibliapi.herokuapp.com/films')
+    puts films_array.parsed_response
     #response.body, response.code, response.message, response.headers.inspect
   end
 
   def self.get_species
-    self.class.get("/species")
+    species_array = HTTParty.get('https://ghibliapi.herokuapp.com/species')
+    puts species_array.parsed_response
   end
 
 end
